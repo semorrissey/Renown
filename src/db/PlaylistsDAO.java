6f09ceb1b88c7af34286fb99b31066b9e3495548
Nj49a <nj49a@sbcglobal.net>
2019-11-25 19:43:52 -0500

    Updated the Daos

--- src/db/PlaylistsDAO.java
+++ src/db/PlaylistsDAO.java
@@ -1,5 +1,50 @@
 package db;
 
-public class PlaylistsDAO {
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import model.*;
+
+public class PlaylistsDAO {
+	
+	java.sql.Connection conn;
+
+	public PlaylistsDAO() {
+		try  {
+			conn = DatabaseUtil.connect();
+		} catch (Exception e) {
+			conn = null;
+		}
+	}
+	
+	public List<Playlist> getAllPlaylists() throws Exception {
+
+		List<Playlist> allPlaylists = new ArrayList<>();
+		try {
+			Statement statement = conn.createStatement();
+			String query = "SELECT * FROM playlists";
+			ResultSet resultSet = statement.executeQuery(query);
+
+			while (resultSet.next()) {
+				Playlist p = generatePlaylist(resultSet);
+				allPlaylists.add(p);
+			}
+			resultSet.close();
+			statement.close();
+			return allPlaylists;
+
+		} catch (Exception e) {
+			throw new Exception("Failed in getting books: " + e.getMessage());
+		}
+	}
+	
+	private Playlist generatePlaylist(ResultSet resultSet) throws Exception {
+		//fix later
+		//String name  = resultSet.getString("name");
+		//Double value = resultSet.getDouble("value");
+		//return new Constant (name, value);
+		return new Playlist();
+	}
 }
--- src/db/SegmentsDAO.java
+++ src/db/SegmentsDAO.java
@@ -1,5 +1,49 @@
 package db;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.*;
+
 public class SegmentsDAO {
 
+	java.sql.Connection conn;
+
+	public SegmentsDAO() {
+		try  {
+			conn = DatabaseUtil.connect();
+		} catch (Exception e) {
+			conn = null;
+		}
+	}
+
+	public List<Segment> getAllSegments() throws Exception {
+
+		List<Segment> allSegments = new ArrayList<>();
+		try {
+			Statement statement = conn.createStatement();
+			String query = "SELECT * FROM segments";
+			ResultSet resultSet = statement.executeQuery(query);
+
+			while (resultSet.next()) {
+				Segment s = generateSegment(resultSet);
+				allSegments.add(s);
+			}
+			resultSet.close();
+			statement.close();
+			return allSegments;
+
+		} catch (Exception e) {
+			throw new Exception("Failed in getting books: " + e.getMessage());
+		}
+	}
+
+	private Segment generateSegment(ResultSet resultSet) throws Exception {
+		//fix later
+		//String name  = resultSet.getString("name");
+		//Double value = resultSet.getDouble("value");
+		//return new Constant (name, value);
+		return new Segment();
+	}
 }
