dadd68285fa8f59d5b955676cdc8143873c60b9e
Sean Morrissey <semorrissey17@gmail.com>
2019-11-26 22:40:25 -0500

    Library functionality updated

--- html/renown.js
+++ html/renown.js
@@ -1,89 +1,27 @@
-// all access driven through BASE. Must end with a SLASH
-// be sure you change to accommodate your specific API Gateway entry point
-var base_url = "https://5b7elq1ly4.execute-api.us-east-2.amazonaws.com/alpha/"; 
+var videoNames = [];
+videoNames.push("FOX1.ogg");
+videoNames.push("KHAN1.ogg");
+videoNames.push("KHAN2.ogg");
+videoNames.push("KIRK1.ogg");
+videoNames.push("KIRK2.ogg");
+videoNames.push("LINDSROM1.ogg");
+videoNames.push("MCCOY1.ogg");
+videoNames.push("SPOK1.ogg");
+videoNames.push("SPOK2.ogg");
+videoNames.push("SULU1.ogg");
 
-var list_segments   = base_url + "listsegments";    // GET
-var list_playlists  = base_url + "listplaylists";    // GET
 
-// generated from aws. BAD IDEA to encode here, but just getting something done.
-// var remote_url = "https://rct351a2w3.execute-api.us-east-1.amazonaws.com/secure/";
-
-// var search_remote_url = remote_url + "remote";
-
-// var apikey = "OJJuRbpDPX7pJdLDOlDwR1SwyOryDpfa5ChT6ZRs";
-
-// test directly 
-// curl -X POST -H "x-api-key: theKey" -H "Content-Type: application/json" -d '{"key":"val"}' https://[api-id].execute-api.[region].amazonaws.com
-/**
- * Refresh constant list from server
- *
- *    GET list_segments
- *    RESPONSE  list of [id, character, line, url, availableRemote] constants 
- */
-/**
- * Refresh constant list from server
- *
- *    GET list_segments
- *    RESPONSE  list of [id, character, line, url, availableRemote] constants 
- */
-function refreshConstantsList() {
-   var xhr = new XMLHttpRequest();
-   xhr.open("GET", list_segments, true);
-   xhr.send();
-   
-   console.log("sent");
-
-  // This will process results and update HTML as appropriate. 
- /* xhr.onloadend = function () {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      console.log ("XHR:" + xhr.responseText);
-      processListResponse(xhr.responseText);
-    } else {
-      processListResponse("N/A");
-    }
-  };*/
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-      console.log ("XHR:" + xhr.responseText);
-       return processListResponse(xhr.responseText);
-    } else {
-      return processListResponse("N/A");
-    }
-}
-
-/**
- * Respond to server JSON object.
- *
- * Replace the contents of 'segmentList' with a <br>-separated list of id,character,line,url,availableRemote objects.
- */
-function processListResponse(result) {
-  console.log("res:" + result);
-  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
-  var js = JSON.parse(result);
-  
-  var output = "";
-  for (var i = 0; i < js.list.length; i++) {
-    var segmentJson = js.list[i];
-    console.log(segmentJson);
-    
-    var sid = segmentJson["id"];
-    var scharacter = segmentJson["character"];
-    var sline = segmentJson["line"];
-    var surl = segmentJson["url"];
-    var savailableRemote = segmentJson["availableRemote"];
-    var sysvar = segmentJson["system"];
-  }
-
-  // Update computation result
-    return surl;
-}
 
 function addVideos(){
+    var i = 0;
+    for(i = 0; i<videoNames.length;i++){
     var videoElement = document.createElement('video');
-    document.getElementById('video').appendChild(videoElement);
+    document.getElementById('tb').appendChild(videoElement);
     var source = document.createElement('source');
-    source.src = refreshConstantsList();
+    source.src = videoNames[i];
     source.type = "type/ogg";
     videoElement.appendChild(source);
+    }
 }
 
 var playListNames = [];
