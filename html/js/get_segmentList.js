bf98b2c9f00dcebb5aa8d2bad6f33e9b08f8bdc8
Sean Morrissey <semorrissey17@gmail.com>
2019-12-04 14:12:41 -0500

    added aws integration to web app

--- html/js/get_segmentList.js
+++ html/js/get_segmentList.js
@@ -1,26 +1,11 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = "https://5b7elq1ly4.execute-api.us-east-2.amazonaws.com/alpha"; 
+var base_url = "https://5b7elq1ly4.execute-api.us-east-2.amazonaws.com/alpha/"; 
 
 var list_segments   = base_url + "listsegments";    // GET
 var list_playlists  = base_url + "listplaylists";    // GET
 
-// generated from aws. BAD IDEA to encode here, but just getting something done.
-// var remote_url = "https://rct351a2w3.execute-api.us-east-1.amazonaws.com/secure/";
-
-// var search_remote_url = remote_url + "remote";
-
-// var apikey = "OJJuRbpDPX7pJdLDOlDwR1SwyOryDpfa5ChT6ZRs";
-
-// test directly 
-// curl -X POST -H "x-api-key: theKey" -H "Content-Type: application/json" -d '{"key":"val"}' https://[api-id].execute-api.[region].amazonaws.com
-/**
- * Refresh constant list from server
- *
- *    GET list_segments
- *    RESPONSE  list of [id, character, line, url, availableRemote] constants 
- */
-function refreshConstantsList() {
+function refreshSegmentsList() {
    var xhr = new XMLHttpRequest();
    xhr.open("GET", list_segments, true);
    xhr.send();
@@ -31,9 +16,9 @@ function refreshConstantsList() {
   xhr.onloadend = function () {
     if (xhr.readyState == XMLHttpRequest.DONE) {
       console.log ("XHR:" + xhr.responseText);
-      processListResponse(xhr.responseText);
+       return processListResponse(xhr.responseText);
     } else {
-      processListResponse("N/A");
+     return processListResponse("N/A");
     }
   };
 }
@@ -49,10 +34,11 @@ function processListResponse(result) {
   var js = JSON.parse(result);
   var segmentList = document.getElementById('segmentList');
   
-  var output = "";
+  var output = [js.list.length];
   for (var i = 0; i < js.list.length; i++) {
     var segmentJson = js.list[i];
-    console.log(segmentJson);
+    output[i] = js.list[i];
+    //console.log(segmentJson);
     
     var sid = segmentJson["id"];
     var scharacter = segmentJson["character"];
@@ -60,15 +46,10 @@ function processListResponse(result) {
     var surl = segmentJson["url"];
     var savailableRemote = segmentJson["availableRemote"];
     var sysvar = segmentJson["system"];
-    if (sysvar) {
-    	output = output + "<div id=\"seg" + sid + "\"><b>" + sid + ":</b> = " + scharacter + sline + surl + savailableRemote + "<br></div>";
-    } else {
-    	output = output + "<div id=\"seg" + sid + "\"><b>" + sid + ":</b> = " + scharacter + sline + surl + savailableRemote + "(<a href='javaScript:requestDelete(\"" + sid + "\")'><img src='deleteIcon.png'></img></a>) <br></div>";
-    }
+
   }
 
   // Update computation result
-  segmentList.innerHTML = output;
     return output;
 }
 
--- html/renown.js
+++ html/renown.js
@@ -1,26 +1,140 @@
 var videoNames = [];
-videoNames.push("FOX1.ogg");
-videoNames.push("KHAN1.ogg");
-videoNames.push("KHAN2.ogg");
-videoNames.push("KIRK1.ogg");
-videoNames.push("KIRK2.ogg");
-videoNames.push("LINDSROM1.ogg");
-videoNames.push("MCCOY1.ogg");
-videoNames.push("SPOCK1.ogg");
-videoNames.push("SPOCK2.ogg");
-videoNames.push("SULU1.ogg");
+var charNames = [];
+var phrases = [];
+var playListNames = [];
+
+var dataList;
+
+// all access driven through BASE. Must end with a SLASH
+// be sure you change to accommodate your specific API Gateway entry point
+var base_url = "https://5b7elq1ly4.execute-api.us-east-2.amazonaws.com/alpha/"; 
+
+var list_segments   = base_url + "listsegments";    // GET
+var list_playlists  = base_url + "listplaylists";    // GET
+
+function refreshSegmentsList() {
+   var xhr = new XMLHttpRequest();
+   xhr.open("GET", list_segments, true);
+   xhr.send();
+   
+   console.log("sent");
+
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log ("XHR:" + xhr.responseText);
+       return processListResponse(xhr.responseText);
+    } else {
+     return processListResponse("N/A");
+    }
+  };
+}
+
+function refreshPlaylistsList() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", list_playlists, true);
+    xhr.send();
+    console.log("sent");
+    
+  // This will process results and update HTML as appropriate. 
+  xhr.onloadend = function () {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+      console.log ("XHR:" + xhr.responseText);
+      processPlaylistResponse(xhr.responseText);
+    } else {
+      processPlaylistResponse("N/A");
+    }
+  };
+}
+
+
+/**
+ * Respond to server JSON object.
+ *
+ * Replace the contents of 'segmentList' with a <br>-separated list of id,character,line,url,availableRemote objects.
+ */
+function processListResponse(result) {
+  console.log("res:" + result);
+  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+  var js = JSON.parse(result);
+  var segmentList = document.getElementById('segmentList');
+  
+  var output = [js.list.length];
+  for (var i = 0; i < js.list.length; i++) {
+    var segmentJson = js.list[i];
+    output[i] = js.list[i];
+    //console.log(segmentJson);
+    
+    var sid = segmentJson["id"];
+    var scharacter = segmentJson["character"];
+    var sline = segmentJson["line"];
+    var surl = segmentJson["url"];
+    var savailableRemote = segmentJson["availableRemote"];
+    var sysvar = segmentJson["system"];
+
+    videoNames.push(surl);
+      charNames.push(scharacter);
+      phrases.push(sline);
+  }
+    datalist = js.list;
+  // Update computation result
+    return output;
+    
+}
+
+function processPlaylistResponse(result) {
+  console.log("res:" + result);
+  // Can grab any DIV or SPAN HTML element and can then manipulate its contents dynamically via javascript
+  var js = JSON.parse(result);
+  var segmentList = document.getElementById('segmentList');
+  
+  var output = "";
+  for (var i = 0; i < js.list.length; i++) {
+    var segmentJson = js.list[i];
+    console.log(segmentJson);
+    
+    var pname = segmentJson["name"];
+    var pseg_id = segmentJson["seg_id"];
+    var pseg_order = segmentJson["seg_order"];
+    var sysvar = segmentJson["system"];
+      if(!playListNames.includes(pname)){
+    playListNames.push(pname);
+      }
+  }
+
+  // Update computation result
+    return output;
+}
+
 
 function removeVideos(){
     var myNode = document.getElementById("tb");
   while (myNode.firstChild) {
     myNode.removeChild(myNode.firstChild);
   }
+    videoNames = [];
+    refreshSegmentsList();
+}
+
+function removePlaylists(){
+      var myNode = document.getElementById("tb");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+    playListNames = [];
+    refreshPlaylistsList();
 }
 
 function addVideos(){
-    removeVideos();
+    removePlaylists();
     var i ;
     for(i = 0; i<videoNames.length;i++){
+    var name = document.createElement('p');
+        name.innerHTML = charNames[i];
+    var phrase = document.createElement('p');
+        phrase.innerHTML = phrases[i];
+    document.getElementById('tb').appendChild(name);
+    document.getElementById('tb').appendChild(phrase);
     var videoElement = document.createElement('video');
     var source = document.createElement('source');
     source.src = videoNames[i];
@@ -31,9 +145,9 @@ function addVideos(){
     videoElement.append(source);
     document.getElementById('tb').appendChild(videoElement);
     }
+    console.log(videoNames);
 }
 
-var playListNames = [];
 
 function getPlaylistNames(){
     var nameOfPlaylist = document.getElementById('playlistName').value;
@@ -45,15 +159,18 @@ function getPlaylistNames(){
 
 function showPlaylists(){  
     removeVideos();
-var videoElement = document.getElementById('video');
-if(videoElement){
-videoElement.remove();
-}
  var i;
         for(i = 0; i<playListNames.length; i++){
             var tabs = document.getElementById('tb');
-            var x = document.createElement("P");
+            var x = document.createElement("p");
             x.innerHTML = playListNames[i];
             tabs.appendChild(x);
         }
 } 
+
+window.onload = function() {
+    refreshSegmentsList();
+    refreshPlaylistsList();
+}
+
+
